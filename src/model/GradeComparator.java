3f8358c143683385c3fa7e9f9e484da8f6711a7b
JoseAvila03 <166460706+JoseAvila03@users.noreply.github.com>
2025-04-24 14:10:26 -0700

    Update GradeComparator.java

--- src/model/GradeComparator.java
+++ src/model/GradeComparator.java
@@ -1,20 +1,18 @@
 package model;
-
 import java.util.Comparator;
 
 public class GradeComparator implements Comparator<Student> {
+    private final String courseName;
 
-	//Comparator probably doesnt work, still havent updated Student class
-	//Maybe we need to compare the students with a specific grade of a class?
-	@Override
-	public int compare(Student o1, Student o2) {
-		if(o1.getAverageGrade() > o2.getAverageGrade()) {
-			return 1;
-		}
-		else if(o1.getAverageGrade() < o2.getAverageGrade()) {
-			return -1;
-		}
-		return 0;
-	}
-	
+    public GradeComparator(String courseName) {
+        this.courseName = courseName;
+    }
+
+    @Override
+    public int compare(Student s1, Student s2) {
+        double grade1 = s1.viewCourseAverage(courseName);
+        double grade2 = s2.viewCourseAverage(courseName);
+
+        return Double.compare(grade1, grade2); // Sort ascending
+    }
 }
