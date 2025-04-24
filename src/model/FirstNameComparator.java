59e104c94429d629431dae03eb1c901d5fc83d0c
JoseAvila03 <166460706+JoseAvila03@users.noreply.github.com>
2025-04-24 14:09:53 -0700

    Update FirstNameComparator.java

--- src/model/FirstNameComparator.java
+++ src/model/FirstNameComparator.java
@@ -4,9 +4,10 @@ import java.util.Comparator;
 
 public class FirstNameComparator implements Comparator<Student>{
 
+	
 	@Override
-	public int compare(Student o1, Student o2) {
-		return o1.username.compareTo(o2.username);
-	}
+    public int compare(Student o1, Student o2) {
+        return o1.getName().compareToIgnoreCase(o2.getName());
+    }
 
 }
